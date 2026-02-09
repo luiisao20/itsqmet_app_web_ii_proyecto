cb520783e93f28cad5c81b552be920082b7d6a37
Lucho <bravo.luis.1995@gmail.com>
2026-02-09 16:22:29 -0500

    Vistas generales y flowbite

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { AboutUs } from './features/about-us/about-us';
+import { Bookings } from './features/bookings/bookings';
+import { ContactUs } from './features/contact-us/contact-us';
+import { Movies } from './features/movies/movies';
+import { NotFound } from './features/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', component: AboutUs },
+  { path: 'booking', component: Bookings },
+  { path: 'contact', component: ContactUs },
+  { path: 'movies', component: Movies },
+
+  { path: '**', component: NotFound },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './shared/navbar/navbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, MatIconModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('cine-app');
