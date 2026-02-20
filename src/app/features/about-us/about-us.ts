003a820f3848ab25955dd99b9bc624996a612860
Gerardo Araque <gerard.araq@gmail.com>
2026-02-19 21:33:32 -0500

    Fix footer Booking / About us

--- src/app/features/about-us/about-us.ts
+++ src/app/features/about-us/about-us.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { Button } from "../../shared/button/button";
-import { MissionVision } from "../../shared/mission-vision/mission-vision";
-import { BenefitsCard } from "../../shared/benefits-card/benefits-card";
-import { Timeline } from "../../shared/timeline/timeline";
-import { Footer } from "../../shared/footer/footer";
+import { Button } from '../../shared/button/button';
+import { MissionVision } from '../../shared/mission-vision/mission-vision';
+import { BenefitsCard } from '../../shared/benefits-card/benefits-card';
+import { Timeline } from '../../shared/timeline/timeline';
 
 interface Benefit {
   title: string;
@@ -14,7 +13,7 @@ interface Benefit {
 
 @Component({
   selector: 'app-about-us',
-  imports: [Button, MissionVision, BenefitsCard, Timeline, Footer],
+  imports: [Button, MissionVision, BenefitsCard, Timeline],
   templateUrl: './about-us.html',
   styleUrl: './about-us.css',
 })
@@ -23,20 +22,23 @@ export class AboutUs {
     {
       title: 'Snacks Gourmet',
       icon: 'ionFastFood',
-      description: 'Disfruta de una amplia variedad de snacks gourmet, desde palomitas de maíz con sabores únicos hasta nachos con queso fundido, pasando por opciones saludables como frutas frescas y ensaladas.',
-      image: '/images/benefit-food.jpg'
+      description:
+        'Disfruta de una amplia variedad de snacks gourmet, desde palomitas de maíz con sabores únicos hasta nachos con queso fundido, pasando por opciones saludables como frutas frescas y ensaladas.',
+      image: '/images/benefit-food.jpg',
     },
     {
       title: 'Sección VIP',
       icon: 'ionDiamond',
-      description: 'Experimenta el lujo de nuestra sección VIP, con asientos reclinables de primera clase, servicio personalizado y acceso exclusivo a áreas de descanso y bares privados.',
-      image: '/images/benefit-vip.jpg'
+      description:
+        'Experimenta el lujo de nuestra sección VIP, con asientos reclinables de primera clase, servicio personalizado y acceso exclusivo a áreas de descanso y bares privados.',
+      image: '/images/benefit-vip.jpg',
     },
     {
       title: 'Tecnología de Punta',
       icon: 'ionTv',
-      description: 'Sumérgete en la mejor experiencia cinematográfica con nuestra tecnología de punta en imagen y sonido, que incluye pantallas de alta definición, sistemas de sonido envolvente y proyección láser.',
-      image: '/images/benefit-technology.jpg'
-    }
-  ]
+      description:
+        'Sumérgete en la mejor experiencia cinematográfica con nuestra tecnología de punta en imagen y sonido, que incluye pantallas de alta definición, sistemas de sonido envolvente y proyección láser.',
+      image: '/images/benefit-technology.jpg',
+    },
+  ];
 }
